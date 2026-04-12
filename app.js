a9ae2c53149354878858612210ceb58b6bbbd86f
Murat Gür <murat50122@gmail.com>
2026-04-12 10:27:53 +0300

    V0.1

---
+++ app.js
@@ -0,0 +1,36 @@
+
+const appetizersMenu = document.querySelector("#appetizersMenu");
+const hotstartersMenu = document.querySelector("#hotstartersMenu");
+
+
+
+appetizersMenu.addEventListener("click", (e) => {
+    e.preventDefault();
+
+});
+
+hotstartersMenu.addEventListener("click", (e)=>{
+    e.preventDefault();
+})
+
+
+
+
+
+
+function sayfaYukle(dosyaAdi) {
+    // fetch ile dosyayı çağırıyoruz
+    fetch(dosyaAdi)
+        .then(response => {
+            // Dosya bulundu mu kontrol et
+            if (!response.ok) throw new Error("Dosya yüklenemedi!");
+            return response.text(); // İçeriği metin (HTML) olarak al
+        })
+        .then(html => {
+            // Gelen HTML içeriğini ana sayfadaki body alanına yerleştir
+            document.getElementById('content-area').innerHTML = html;
+        })
+        .catch(error => {
+            console.error("Hata:", error);
+        });
+}
